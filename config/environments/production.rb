0f929d84e1b937d74dc861a57965787736853b34
tetsuyasuzuki <suzukakousen_suzuki@yahoo.co.jp>
2017-10-23 13:36:07 +0000

    production change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,13 +78,13 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   
-  config.action_mailer.default_url_options = { host: 'facebsamantha..herokuapp.com' }
+  config.action_mailer.default_url_options = { host: '13.115.167.46' }
   ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings =
 {
  user_name: ENV['SENDGRID_USERNAME'],
  password: ENV['SENDGRID_PASSWORD'],
- domain: "heroku.com",
+ domain: "aws.com",
  address: "smtp.sendgrid.net",
  port: 587,
  authentication: :plain,
