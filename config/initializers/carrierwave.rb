cf2f5e29855f5122b81cceccc9fa7616989f44e6
tetsuyasuzuki <suzukakousen_suzuki@yahoo.co.jp>
2017-10-24 10:11:59 +0000

    change

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,7 +12,10 @@ CarrierWave.configure do |config|
 
   case Rails.env
     when 'production'
-      config.fog_directory = 'tesuzuki-faceb-production'
+      config.fog_directory = 'tesuzuki-achieve-production'
       config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/tesuzuki-faceb-production'
+    when 'development'
+      config.fog_directory = 'tesuzuki-achieve-production'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/tesuzuki-achieve-production'
   end
 end
