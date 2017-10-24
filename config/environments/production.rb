6cc4f478465787ed36e36e61ff590a02aa598fb6
tetsuyasuzuki <suzukakousen_suzuki@yahoo.co.jp>
2017-10-24 04:03:43 +0000

    wakarann

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,16 +78,16 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   
-  config.action_mailer.default_url_options = { host: '13.115.167.46' }
+  config.action_mailer.default_url_options = { host: 'calm-island-89669.herokuapp.com' }
   ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings =
-{
- user_name: ENV['SENDGRID_USERNAME'],
- password: ENV['SENDGRID_PASSWORD'],
- domain: "",
- address: "smtp.sendgrid.net",
- port: 587,
- authentication: :plain,
- enable_starttls_auto: true
-}
+  ActionMailer::Base.smtp_settings =
+    {
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: "heroku.com",
+    address: "smtp.sendgrid.net",
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+    }
 end
